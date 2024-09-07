73de002238329569dd6d0fe7270c98712d646296
GuiHLima91 <guilhlima1991@gmail.com>
2024-09-06 22:34:38 -0300

    Add files via upload

---
+++ nome.js
@@ -0,0 +1,20 @@
+let nome = [
+    {
+        titulo: "Ana",
+        descricao: "Ana é um nome de origem hebraica, que significa cheia de graça ou favorita de Deus. É um nome clássico e popular em diversas culturas.",
+        link: "https://www.significados.com.br/ana/",
+        tags: "Aninha Nana",
+    },
+    {
+        titulo: "Sofia",
+        descricao: "Sofia é um nome de origem grega, que significa sabedoria. É um nome elegante e atemporal, muito popular em diversos países.",
+        link: "https://www.significados.com.br/sofia/",
+        tags: "Sofi fia",
+    },
+    {
+        titulo: "Joana",
+        descricao: "Joana é um nome de origem hebraica e possui um significado que remete à \"Deus é gracioso\" ou \"agraciada por Deus\".",
+        link: "https://revistacrescer.globo.com/guia-de-nomes/joana/",
+        tags: "Jô Jojo",
+    }
+];
